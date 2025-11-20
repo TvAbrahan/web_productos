5803cac5eb5cea1da833f26819bd1d56e7d79f94
Abrahan Tuñon <abrahan.tunon@utp.ac.pa>
2025-11-19 20:06:09 -0500

    Configurando la pagina

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/web_productos/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
