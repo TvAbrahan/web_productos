8d4c9db9430b7ed36982c6818fd2cabaf570cc11
Abrahan Tuñon <abrahan.tunon@utp.ac.pa>
2025-11-19 20:26:49 -0500

    Rutas para el despliegue

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../App.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory('#'),
   routes: [
     {
       path: '/',
