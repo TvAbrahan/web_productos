7847b897d9a87c5a6a8effc31816ecc083790ab2
Abrahan Tuñon <abrahan.tunon@utp.ac.pa>
2025-11-19 20:15:53 -0500

    Agregando de nuevo actualizacion

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../App.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
       name: 'home',
       component: HomeView,
     },
-    {},
   ],
 })
 
